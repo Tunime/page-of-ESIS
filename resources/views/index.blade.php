0cfa1a5104d886b8d7b254685fc9658f3df8b0e1
tunime <tunime.1@gmail.com>
2018-05-23 18:21:56 -0500

    terminando el maquetado para mobiles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,38 +13,121 @@
   </head>
   <body>
   <!--Comensamos la barra de navegacion-->
-  <div class="container-fluid">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Acerca de</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Plana Docente</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Novedades</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Alumno</a>
-                </li>
-                </ul>
+    <!--<div class="cocontainer-fluid bg-dark">
+            <div class="container bg-dark">
+                <nav class=" navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="collapse navbar-collapse d-flex justify-content-end">
+                        <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a style="padding: .0 1rem;" class="nav-link" href="#">Los damascosavenida</a>
+                        </li>
+                        <li class="nav-item">
+                            <a style="padding: .0 0;" class="nav-link" href="#">+51 089 986 298</a>
+                        </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>-->
+    <div class="container-fluid bg-white">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                <a class="navbar-brand" href="#">
+                    <img class="menu__logo" src="images/logounjbg.png" height="50" alt="logo-unjbg">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Acerca de</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Plana Docente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Novedades</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Alumno
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>  
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
-  </div>
-  <!--Terminamos la barra de navegacion-->
+    <!--Terminamos la barra de navegacion-->
+    <!--Comienso Header-->
+    <div class="container-fluid backsegundario">
+        <div class="container backsegundario">
+            <div class="header d-flex align-items-center">
+                <span class="">
+                    <h1 class="header__titulo">ESCUELA PROFESIONAL DE<br> INGENIERÍA EN INFORMATICA Y<br> SISTEMAS</h1>
+                    <div class="header__pie backprimario"></div>
+                    <p  class="header__subtitulo">FACULTAD DE INGENIERIA</p>
+                    <a class="header__boton btn--activado" href="#">LEER MAS</a>
+                </span>
+            </div>
+        </div>
+    </div>
+    <!--Fin del header-->
+    <!--Comiensa las noticias-->
+    <div class="container-fluid">
+        <div class="container noticias">
+            <h2 class="presentacion__title">Nuevas Noticias</h2>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+            </article>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+            </article>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+            </article>
+            <a class="header__boton btn--desactivado" href="#">LEER MAS</a>
+        </div>
+    </div>
+    <!--Fin de las noticias-->
+    <!--Comienso de eventos-->
+    <div class="container-fluid backsegundario">
+        <div class="container eventos backsegundario">
+            <h2 class="presentacion__title">Nuevas Noticias</h2>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+            </article>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+            </article>
+            <article class="noticias__item">
+                <time class="item__fecha">23 noviembre 2018</time>
+                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+            </article>
+            <a class="" href="#">LEER MAS ...</a>
+        </div>
+    </div>
+    <!--Fin de eventos-->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="js/mocatemplante.js"></script>
   </body>
 </html>
