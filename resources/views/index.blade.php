27b5fc5cac7854c4c53f058fca7ab9c6623361d6
tunime <tunime.1@gmail.com>
2018-05-23 21:02:08 -0500

    añadiendo las imagenes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,23 +40,33 @@
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Acerca de</a>
-                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Acerca de
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Vision</a>
+                                <a class="dropdown-item" href="#">Mision</a>
+                                <a class="dropdown-item" href="#">Objetivos</a>
+                            </div>
+                        </li> 
                         <li class="nav-item">
                             <a class="nav-link" href="#">Plana Docente</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Novedades</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Biblioteca</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Alumno
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
+                                <a class="dropdown-item" href="#">Conograma Academico</a>
+                                <a class="dropdown-item" href="#">Horarios</a>
+                                <a class="dropdown-item" href="#">Aula Virtual</a>
                             </div>
                         </li>  
                     </ul>
@@ -66,11 +76,11 @@
     </div>
     <!--Terminamos la barra de navegacion-->
     <!--Comienso Header-->
-    <div class="container-fluid backsegundario">
-        <div class="container backsegundario">
+    <div class="container-fluid header__principal">
+        <div class="container">
             <div class="header d-flex align-items-center">
                 <span class="">
-                    <h1 class="header__titulo">ESCUELA PROFESIONAL DE<br> INGENIERÍA EN INFORMATICA Y<br> SISTEMAS</h1>
+                    <h1 class="header__titulo"><span class="text-white">ESCUELA PROFESIONAL DE</span><br> INGENIERÍA EN INFORMATICA Y<br> SISTEMAS</h1>
                     <div class="header__pie backprimario"></div>
                     <p  class="header__subtitulo">FACULTAD DE INGENIERIA</p>
                     <a class="header__boton btn--activado" href="#">LEER MAS</a>
@@ -89,19 +99,40 @@
                         <div class="container noticias">
                             <h2 class="presentacion__title">Nuevas Noticias</h2>
                             <article class="noticias__item">
-                                <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
-                                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                                <div class="row">
+                                    <figure class="col-lg-4">
+                                        <img src="http://www.unjbg.edu.pe/img/2017-7/78451.JPG" alt="..." class="img-thumbnail">
+                                    </figure>
+                                    <div class="col-lg-8">
+                                        <time class="item__fecha">23 noviembre 2018</time>
+                                        <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
+                                        <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                                    </div>
+                                </div>
                             </article>
                             <article class="noticias__item">
-                                <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
-                                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                                <div class="row">
+                                    <figure class="col-lg-4">
+                                        <img src="http://www.unjbg.edu.pe/img/2017-7/78451.JPG" alt="..." class="img-thumbnail">
+                                    </figure>
+                                    <div class="col-lg-8">
+                                        <time class="item__fecha">23 noviembre 2018</time>
+                                        <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
+                                        <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                                    </div>
+                                </div>
                             </article>
                             <article class="noticias__item">
-                                <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
-                                <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                               <div class="row">
+                                    <figure class="col-lg-4">
+                                        <img src="http://www.unjbg.edu.pe/img/2017-7/78451.JPG" alt="..." class="img-thumbnail">
+                                    </figure>
+                                    <div class="col-lg-8">
+                                        <time class="item__fecha">23 noviembre 2018</time>
+                                        <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
+                                        <p class="item__contenido">page when loking at its loyout, the point of using lorem ipsum is that it has a more-or-less normal distribution of etters, as opposed to using content here,content ...</p>
+                                    </div>
+                                </div>
                             </article>
                             <a class="header__boton btn--desactivado" href="#">LEER MAS</a>
                         </div>
@@ -115,15 +146,15 @@
                             <h2 class="presentacion__title">Nuestros eventos</h2>
                             <article class="noticias__item">
                                 <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                                <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
                             </article>
                             <article class="noticias__item">
                                 <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                                <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
                             </article>
                             <article class="noticias__item">
                                 <time class="item__fecha">23 noviembre 2018</time>
-                                <h3 class="item__title">Long Established fact that a reader will be distracted by the readable</h3>
+                                <h3 class="item__title"><a class="text-dark" href="#">Long Established fact that a reader will be distracted by the readable</a></h3>
                             </article>
                             <a class="" href="#">LEER MAS ...</a>
                         </div>
@@ -134,7 +165,57 @@
         </div>
     </div>
     <!--Fin del contenido-->
-    
+    <!--inicio SECCION DE FOOTER-->
+    <footer class="pt-5 footer">
+        <div class="container-fluid backprimario">
+            <div class="container pt-3 backprimario">
+                <div class="row">
+                    <div class="col-md-12 col-lg-4">
+                         <a class="navbar-brand" href="#">
+                            <img class="menu__logo" src="images/logounjbg.png" height="50" alt="logo-unjbg">
+                        </a>
+                        <p class="footer__pie ml-5">Ciudad Universitaria<br>Av.miraflores S/N<br>central telefonico 052-583000<br>Tacna-Peru</p>
+                    </div>
+                    <div class="col-2 d-none d-md-block">
+                        <a class="nav-link footer__menu" href="#">Acerca de</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                    </div>
+                    <div class="col-2 d-none d-md-block">
+                        <a class="nav-link footer__menu" href="#">Acerca de</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                    </div>
+                    <div class="col-2 d-none d-md-block">
+                        <a class="nav-link footer__menu" href="#">Acerca de</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                    </div>
+                    <div class="col-2 d-none d-md-block">
+                        <a class="nav-link footer__menu" href="#">Acerca de</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                        <a class="nav-link footer__menu" href="#">Servicios</a>
+                    </div>
+                </div>
+                <!--<nav class="nav">
+                    <a class="nav-link text-white" href="#">Servicios</a>
+                    <span class="nav-link text-white">/</span>
+                    <a class="nav-link text-white" href="#">Galeria</a>
+                    <span class="nav-link text-white">/</span>
+                    <a class="nav-link text-white" href="#">Experiencia</a>
+                    <span class="nav-link text-white">/</span>
+                    <a class="nav-link text-white" href="#">contactanos</a>
+                </nav>-->
+            </div>
+        </div>
+        <div class="container-fluid bg-dark">
+            <div class="container bg-dark">
+                <div class="footer__pie text-white bg-dark d-flex justify-content-center p-1">ESCUELA PROFESIONAL DE INGENIERÍA EN INFORMATICA Y SISTEMAS</div>
+            </div>
+        </div>
+    </footer>
+<!--fin SECCION DE FOOTER-->
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
